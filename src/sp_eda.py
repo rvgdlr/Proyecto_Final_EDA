022ef8e9f1a2c212c6575fe61e4952692eab5760
rvgdlr <rvgdlr@gmail.com>
2026-02-16 08:56:21 +0000

    Analisis preliminar entero

---
+++ src/sp_eda.py
@@ -0,0 +1,51 @@
+# Tratamiento de datos
+
+import pandas as pd
+
+"""
+Realiza un análisis exploratorio preliminar sobre un DataFrame.
+Este análisis incluye:
+- Muestra aleatoria de 5 filas
+- Información general
+- Porcentaje de valores nulos por columna
+- Conteo de filas duplicadas
+- Distribución de valores para columnas categóricas
+
+Parameters:
+df (pd.DataFrame): DataFrame a analizar
+
+Returns:
+None
+"""
+
+
+def eda_preliminar(df):
+    display(df.sample(5))
+
+    print('-----------')
+    print('DIMENSIONES')
+
+    print(f'Nuestro conjunto de datos presenta un total de {df.shape[0]}) filas y {df.shape[1]} columnas')
+
+    print('-----------')
+    print('INFORMACION')
+
+    display(df.info())
+
+    print('-----------')
+    print('NULOS')
+
+    display(df.isnull().mean()*100)
+
+    print('-----------')
+    print('DUPLICADOS')
+
+    print(f'Tenemos un total de {df.duplicated().sum()} duplicados')
+
+    print('-----------')
+    print('FRECUENCIAS CATEGÓRICAS')
+
+    for col in df.select_dtypes(include='object').columns:
+        print(col.upper())
+        print(df[col].value_counts())
+        print('------')
